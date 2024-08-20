f969e743ff3e37f674e706fb114d290dc2a7bc39
mahaputrailhamawal <mahaputrailhamawal@gmail.com>
2024-08-20 09:55:30 +0700

    add: services

---
+++ src/services/user.services.ts
@@ -0,0 +1,33 @@
+import UserRepository from "../repositories/user.repository"
+import { IUser } from "../entities/user.entity";
+
+const UserServices = {
+    getAllUsers: async () => {
+        try {
+            const users = await UserRepository.getAllUsers();
+            return users;
+        } catch (error) {
+            console.log(`Error while getting all users: ${error}`);
+        }
+    },
+
+    getUser: async (id: string) => {
+        try {
+            const user = await UserRepository.getUser(id);
+            return user;
+        } catch (error) {
+            console.log(`Error while getting user: ${error}`);
+        }
+    },
+
+    createUser: async (user: IUser) => {
+        try {
+            const newUser = await UserRepository.createUser(user);
+            return newUser;
+        } catch (error) {
+            console.log(`Error while creating user: ${error}`);
+        }
+    }
+};
+
+export default UserServices;
